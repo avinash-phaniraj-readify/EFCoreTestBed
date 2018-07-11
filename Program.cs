6cf0f6195c22afc64843d3bc75a58a0bc786b64d
Avinash Phaniraj <avinash.phaniraj@readify.net>
2018-07-11 12:03:57 +1000

    test bed.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.SqlServerCe;
+using System.Linq;
+
+namespace TestHostForCastException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceConnectionString = "Data Source=TestDb.sdf; Persist Security Info = False; ";
+            var ceConnection = new SqlCeConnection(ceConnectionString);
+            ceConnection.Open();
+
+            var options = new DbContextOptionsBuilder<TestDataContext>()
+                .UseSqlCe(ceConnection)
+                .Options;
+
+            var context = new TestDataContext(options);
+            //context.Set<Employee>().ToList();
+            //context.Set<EmployeeDevice>().ToList();
+        }
+    }
+}
