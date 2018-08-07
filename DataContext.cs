30a2278ed3db8deb9a1e2da7d1c457b971f4eece
Malik <mnmb1988@hotmail.com>
2018-08-07 16:32:48 +1000

    Building the Model

--- DataContext.cs
+++ DataContext.cs
@@ -1,47 +1,81 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TestHostForCastException
 {
     public class TestDataContext : DbContext
     {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Asset> Assets { get; set; }
+        public DbSet<Asset> Providers { get; set; }
+
         public TestDataContext(DbContextOptions options) : base(options)
         {
-
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<EmployeeDevice>()
-                .HasOne<Employee>(a => a.Employee)
-                .WithMany(p => p.Devices)
-                .HasForeignKey(a => a.EmployeeId)
-                .IsRequired(true);
-
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Asset>()
+           .HasDiscriminator<int>("AssetType")
+           .HasValue<Product>(1);
         }
     }
 
-    public interface IEmployee
-    {
-        string Name { get; set; }
-    }
-    public class Employee : IEmployee
+    [Table("Assets")]
+    public abstract class Asset
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<EmployeeDevice> Devices { get; set; }
+        public string Description { get; set; }
+
+        public int AssetType { get; set; }
+        public int ProviderId { get; set; }
+        public TaxCategory TaxCategory { get; set; }
     }
 
-    public class EmployeeDevice
+    public class Product : Asset
+    {
+        public decimal Price { get; set; }
+    }
+
+    [Table("Providers")]
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Table("TaxCategories")]
+    public class TaxCategory
     {
         [Key]
         public int Id { get; set; }
-        public int EmployeeId { get; set; }
-        public string Device { get; set; }
-        public Employee Employee { get; set; }
+
+        public string Code { get; set; }
+
+        public decimal Percentage { get; set; }
+
+        public int? SerialCategory { get; set; }
     }
 
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+using System;
+using System.Data.SqlClient;
 using System.Data.SqlServerCe;
 using System.Linq;
 
@@ -8,17 +12,53 @@ namespace TestHostForCastException
     {
         static void Main(string[] args)
         {
-            var ceConnectionString = "Data Source=TestDb.sdf; Persist Security Info = False; ";
+            var ceConnectionString = "data source=.;initial catalog=TestingDataBase;integrated security=True;MultipleActiveResultSets=True;";
             var ceConnection = new SqlCeConnection(ceConnectionString);
-            ceConnection.Open();
 
-            var options = new DbContextOptionsBuilder<TestDataContext>()
-                .UseSqlCe(ceConnection)
-                .Options;
 
-            var context = new TestDataContext(options);
-            //context.Set<Employee>().ToList();
-            //context.Set<EmployeeDevice>().ToList();
+            var options = new DbContextOptionsBuilder()
+                .UseSqlCe(ceConnection);
+
+            Func<DbContextOptionsBuilder, DbContextOptionsBuilder> configureOptions = (c) => c.UseLoggerFactory(new LoggerFactory(new[] { new DebugLoggerProvider((_, __) => true) }));
+
+
+            options = configureOptions(options);
+
+            using (var context = new TestDataContext(options.Options))
+            {
+                var assetQuery = from asset in context.Assets.OfType<Product>()
+                                 where asset.TaxCategory.SerialCategory.GetValueOrDefault() != (int)ProvicerTypes.Small
+                                 select asset;
+
+                var productInfo = from  provider in context.Providers 
+                                  join  l in assetQuery on provider.Id equals l.ProviderId
+                                  select provider;
+
+                var transactions = from transaction in context.Transactions
+                                   join l in productInfo on transaction.ProductId equals l.Id
+                                   let cache = l.TaxCategory.SerialCategory
+                                   select new EE
+                                   {
+                                       Total = transaction.TotalAmount,
+                                       Tax = (transaction.TotalAmount) * (l.TaxCategory.Percentage/100)
+                                   };
+
+                var s = transactions.ToList();
+
+                }
         }
+
+        public enum ProvicerTypes
+        {
+            Large = 1,
+            Small = 0
+        }
+
+    }
+    
+    public class EE
+    {
+        public decimal Tax { get; set; }
+        public decimal Total { get; set; }
     }
 }
