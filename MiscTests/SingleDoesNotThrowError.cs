a823ba12c5de611e6e12463d2ed7479d36dc3c94
Avinash Phaniraj <avinash.phaniraj@readify.net>
2018-09-19 14:33:25 +1000

    Added test around single.

---
+++ MiscTests/SingleDoesNotThrowError.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Linq2SqlEFCoreBehaviorsTest.MiscTests
+{
+    public class SingleDoesNotThrowError : TestBase
+    {
+        public SingleDoesNotThrowError(DatabaseFixture fixture) : base(fixture)
+        {
+
+        }
+
+        [Fact]
+        public void Linq2SqlTest()
+        {
+            base.Linq2SqlContext(context => {
+                var results = context.GetTable<Linq2Sql.Employee>()
+                .Select(employee => new { Result = employee.EmployeeDevices.Single(s=> s.Id < 0) })
+                .ToList();
+
+                Assert.True(results.Any());
+                Assert.True(results.All(a=> a.Result == null));
+            });
+        }
+
+        [Fact]
+        public void EFCoreTest()
+        {
+            base.EFContext(context => {
+
+                var exception = Assert.Throws<InvalidOperationException>(() =>
+                {
+                    var results = context.Set<EFCore.Employee>()
+                    .Select(employee => new { Result = employee.Devices.Single(s => s.Id < 0) })
+                    .ToList();
+                });
+
+                Assert.Equal("Sequence contains no elements", exception.Message);
+            });
+        }
+
+        [Fact]
+        public void EFCoreTest_Fixed()
+        {
+            base.EFContext(context => {
+
+                var results = context.Set<EFCore.Employee>()
+                   .Select(employee => new { Result = employee.Devices.FirstOrDefault(s => s.Id < 0) })
+                   .ToList();
+
+                Assert.True(results.Any());
+                Assert.True(results.All(a => a.Result == null));
+            });
+        }
+    }
+}
